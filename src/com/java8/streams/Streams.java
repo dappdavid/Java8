a4e1959bf708d653bdf0da1111d45b949f7b2dc0
dappdavid <dappdavid99@gmail.com>
2022-12-27 09:46:33 +0530

    updated

--- src/com/java8/streams/Streams.java
+++ src/com/java8/streams/Streams.java
@@ -25,6 +25,20 @@ public class Streams {
 		productsList.add(new Product(4, "Sony Laptop", 28000f));
 		productsList.add(new Product(5, "Apple Laptop", 90000f));
 
+		Map<Integer, Product> productMap = new HashMap<>();
+		productMap.put(1, new Product(1, "HP Laptop", 25000f));
+		productMap.put(2, new Product(2, "Dell Laptop", 29000f));
+		productMap.put(3, new Product(3, "Sony Laptop", 35000f));
+
+//		for (Map.Entry<Integer, Product> entry : productMap.entrySet()) {
+//			Integer age = entry.getKey();
+//			Product p = entry.getValue();
+//			System.out.println("age : " + age + " Price: " + p.price);
+//		}
+
+		//Iterate over maps
+		productMap.forEach((age, p) -> System.out.println("age : " + age + " Price: " + p.price));
+		
 		// Without Stream
 		List<Float> productPriceList = new ArrayList<Float>();
 		for (Product product : productsList) {
@@ -41,37 +55,42 @@ public class Streams {
 				.map(p -> p.price) // fetching price
 				.collect(Collectors.toList()); // collecting as list
 		System.out.println(productPriceList2);
-		
-		//forEach
+
+		System.out.println("Filtered by price");
+		productsList.stream().filter(p -> p.price > 25000f).collect(Collectors.toList())
+				.forEach(s -> System.out.println(s.price));
+
+		// forEach
 		Random random = new Random();
 		random.ints().limit(10).forEach(System.out::println);
-		
-		//map
+
+		// map
 		List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
-		//get list of unique squares
-		List<Integer> squaresList = numbers.stream().map( i -> i*i).distinct().collect(Collectors.toList());
-		
-		//filter
-		List<String>strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
-		//get count of empty string
+		// get list of unique squares
+		List<Integer> squaresList = numbers.stream().map(i -> i * i).distinct().collect(Collectors.toList());
+		// Sum of all the elements
+		Integer sum = numbers.stream().reduce(0, (a, b) -> a + b);
+
+		// filter
+		List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
+		// get count of empty string
 		long count = strings.stream().filter(string -> string.isEmpty()).count();
-		
-		//limit
+
+		// limit
 		Random random2 = new Random();
 		random2.ints().limit(10).forEach(System.out::println);
-		
-		//sorted
+
+		// sorted
 		Random random3 = new Random();
 		random3.ints().limit(10).sorted().forEach(System.out::println);
-		
-		//parallelprocessing
-		List<String> strings2 = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
-		//get count of empty string
+
+		// parallelprocessing
+		List<String> strings2 = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
+		// get count of empty string
 		long count2 = strings2.parallelStream().filter(string -> string.isEmpty()).count();
-		
-		
-		//collectors
-		List<String>strings3 = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+
+		// collectors
+		List<String> strings3 = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
 		List<String> filtered = strings3.stream().filter(string -> !string.isEmpty()).collect(Collectors.toList());
 
 		System.out.println("Filtered List: " + filtered);
